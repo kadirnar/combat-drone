4bbec9cf936520b62d3c450c98e433d50e12e49f
Kadir Nar <kadir.nar@hotmail.com>
2021-01-17 13:30:45 +0300

    Add files via upload

---
+++ split-videos-to-frames.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+vidcap = cv2.VideoCapture('video/uav.mp4')
+path_to_save = 'proje'
+
+
+
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+          name = 'frame' + str(count) + '.jpg'
+          cv2.imwrite(os.path.join(path_to_save, name), image)
+        
+    return hasFrames
+
+
+sec = 5
+frameRate = 0.75 
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
