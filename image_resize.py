f5208daf7ea1bba1d7e8b9ed0a3d9906b9a43608
Kadir Nar <kadir.nar@hotmail.com>
2021-02-01 02:45:01 +0300

    Update image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -7,5 +7,5 @@ import glob
 images = [file for file in os.listdir() if file.endswith(('jpeg', 'png', 'jpg'))]
 for image in images:
     img = Image.open(image)
-    img.thumbnail((640,640))
-    img.save("resized_"+image, optimize=True, quality=40)
+    img.thumbnail((640,480))
+    img.save("resized_"+image, optimize=True, quality=80)
